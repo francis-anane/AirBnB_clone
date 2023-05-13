8c70d6a8071976f44b52a7d6d899cd82e52e2ec4
ChalaLe <clelisa28@gmail.com>
2023-05-13 01:48:16 -0700

    user module added

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class for managing user objects.
+
+    This class inherits from the BaseModel class and includes attributes
+    for email, password, first name, and last name. These attributes represent
+    the data associated with a user object.
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
