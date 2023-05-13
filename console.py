b8afd6e449535e54d1fabdab006c0e3e8ead910c
Francis Ofori Anane <ofori.anane@gmail.com>
2023-05-13 15:50:14 +0000

    Update console.py

--- console.py
+++ console.py
@@ -100,7 +100,46 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, command):
         """All: displays string representation of available class instances,
-        an optional argument [class name] can also be specified"""
+        an optional argument [class name] can also be specified display instance
+        of the class
+        """
+
+        # Get saved objects from storage
+        models = storage.all()
+
+        if not command or command == "BaseModel":
+            for key in models.keys():
+                print(BaseModel(**models[key]))
+        else:
+            if command != "BaseModel":
+                print("** class doesn't exist **")
+
+    def do_update(self, command):
+        """Update: update an object base onthe  class name and id
+        by adding and attribute or updating an attribute,
+        save the change in a JSON file
+        """
+
+        if not command:
+            print("** class name missing **")  # No argument given
+        else:
+            # Extract arguments from command line
+            args = command.split()
+            if len(args) == 1:
+                print("** instance id missing **")
+            elif len(args) == 2:
+                print("** attribute name missing **")
+            elif len(args) == 3:
+                print("** value missing **")
+            else:
+                # get objects from storage
+                models = storage.all()
+                # create key from <class name> and <id>
+                key = f"{args[0]}.{args[1]}"
+                if args[0] != "BaseModel":
+                    print("** class doesn't exist **")
+                elif key not in models.keys():
+                    print("** no instance found **")
 
 
 if __name__ == "__main__":
